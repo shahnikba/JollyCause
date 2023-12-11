dd530dccd9e2584912ccfec0bb681608c94deff1
shahram <nikbakhtian@gmail.com>
2023-12-11 23:25:48 +0000

    renamed method

--- JollyCause/CausalInference/DiffMean.py
+++ JollyCause/CausalInference/DiffMean.py
@@ -1,9 +1,8 @@
 import pandas as pd
 import statsmodels as st
 import numpy as np
-df = pd.read_csv("https://docs.google.com/uc?id=1AQva5-vDlgBcM_Tv9yrO8yMYRfQJgqo_&export=download")
 
-def simple_causality(df,w,y):
+def ate_diff_mean(df,w,y):
     # Only valid in the randomized setting. Do not use in observational settings.
     Y = df[y]
     W = df[w]
@@ -13,4 +12,3 @@ def simple_causality(df,w,y):
     ate_pvalue = 2 * (1 - np.abs(ate_est / ate_se))
     ate_results = {"estimate": ate_est, "std.error": ate_se, "t.stat": ate_tstat, "pvalue": ate_pvalue}
     print(ate_results)
-simple_causality(df, "w", "y")
