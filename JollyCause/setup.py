56402d375622d8eeb7bb649b185e7563a328d055
shahram <nikbakhtian@gmail.com>
2023-12-12 16:48:47 +0000

    added setup.py

--- JollyCause/setup.py
+++ JollyCause/setup.py
@@ -1,26 +1,45 @@
-import setuptools
 
-#with open("README.md", "r", encoding = "utf-8") as fh:
-#    long_description = fh.read()
+# -*- coding:utf-8 -*-
+"""Setuptools of jollycause"""
+import setuptools
+import sys
+
+#from xx import __version__
+__version__ = "0.0.1"
+
+if sys.version_info < (3, 6):
+    sys.exit("Sorry, Python < 3.6 is not supported.")
+
+
+with open("README.md", "r", encoding='utf-8') as fh:
+    long_desc = fh.read()
+
 
 setuptools.setup(
-    name = "JollyCause",
-    version = "0.0.1",
-    author = "Dataism",
-    author_email = "author@example.com",
-    description = "short package description",
-    long_description = "long_description",
-    long_description_content_type = "text/markdown",
-    url = "package URL",
-    project_urls = {
-        "Bug Tracker": "package issues URL",
-    },
-    classifiers = [
+    name="jollycause",
+    version=__version__,
+    include_package_data=True,
+    python_requires=">=3.6",
+    author="Shahram Nikbakhtian",
+    author_email="example1@dataism.com",
+    description="JullyCause is the fundamental package for causal structure learning with Python.",
+    long_description=long_desc,
+    long_description_content_type="text/markdown",
+    url="https://github.com/shahnikba/JollyCause",
+    packages=setuptools.find_packages('.', exclude=['web']),
+    license="Apache License 2.0",
+    classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
     ],
-    package_dir = {"": "."},
-    packages = setuptools.find_packages(where="."),
-    python_requires = ">=3.6"
+    install_requires=[
+        "tqdm>=4.48.2",
+        "numpy>=1.19.1",
+        "pandas>=0.22.0",
+        "scipy>=1.7.3",
+        "scikit-learn>=0.21.1",
+        #"matplotlib>=2.1.2",
+        "networkx>=2.5",
+    ],
 )
